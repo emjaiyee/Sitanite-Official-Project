6c1a2116c350c19a6c0b2d156074e7c90ac1f5f8
Jrfire <jrpea059@gmail.com>
2026-09-13 13:35:14 +0800

    Enemy AI Animation
    Animator
    Animation Clip
    Projectile Scripts (Missile, Splash)
    Sprite Library

---
+++ Assets/Scripts/EnemyScripts/Range/Projectiles/IProjectileType.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public interface IProjectileType
+{
+    void Launch(
+        Vector3 direction,
+        int damage,
+        DamageType damageType,
+        float speed,
+        float lifetime);
+}
